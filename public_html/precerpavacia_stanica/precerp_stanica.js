2a478b434748d31e9810f7f6d1d9ed873e5206f9
Chovancova <chovancova.olga@yahoo.com>
2015-03-12 11:00:52 +0100

    interface priblizny

--- public_html/precerpavacia_stanica/precerp_stanica.js
+++ public_html/precerpavacia_stanica/precerp_stanica.js
@@ -1,23 +1,50 @@
-function ComponentTank(fillPercentage){
+function ComponentTank(a, fillPercentage){
     
     
 }
 
-function ComponentValve(isOpen){
+function ComponentValve(a, isOpen){
     
     
 }
 
-function ComponetPipe(){
+function ComponetPipe(a){
     
 }
 
-function ComponetEngine(isOn){
+function ComponetEngine(a, isOn){
     
     
 }
+
+var nadrz, motor, pipe, valve;
 
 function initSchema01(){
     
-    
+    var schema01Paper = new Snap("#svg");
+    /*
+     * nacitanie jednotlivych komponentov cez funkcie / a cez svg.. 
+     * 
+     */
+}
+
+function updateSchema01(hodnota1, hodnota2, hodnota3){
+    /*
+     * volam z atributu funkciu 
+     * napr valveIn.setOpened(isOpened);
+     */
+        
+}
+
+function onPageLoad(){
+    initSchema01();
+}
+
+
+//////////na overenie funkcnosti / bud cez prikazovy riadok javascriptu alebo tlacidlami
+function toggleDemoStart(){
+    var button = document.getElementById("demo");
+    /*
+     * volanie funkcie updateSchema01 s roznymi parametrami a podmienkami if...
+     */
 }
