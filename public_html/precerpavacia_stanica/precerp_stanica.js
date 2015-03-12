ccef8b9695529a288d91f71a576bc3fe96902d63
Chovancova <chovancova.olga@yahoo.com>
2015-03-12 11:31:35 +0100

    male zmeny

--- public_html/precerpavacia_stanica/precerp_stanica.js
+++ public_html/precerpavacia_stanica/precerp_stanica.js
@@ -1,31 +1,42 @@
-function ComponentTank(a, fillPercentage){
-    
-    
+function ComponentTank(a, s, fillPerc, cislo1, cislo2, rychlostVMs) {
+    /*
+     * nacitanie valve zo svg, podla id a / 
+     * zistenie cisla1, cisla2 zo svg
+     */
+    this.rychlostVMs = 800;
+    this.fillPerc = fillPerc;
+    this.animateTank = function animateTank(fillPerc) {
+        var vyska = cislo1 * (perc / 100);
+        var py = cislo2 - vyska;
+        s.select(a).animate({height: vyska, y: py, x: cislo1}, rychlostVMs);
+    };
+
+
 }
 
-function ComponentValve(a, isOpen){
+function ComponentValve(a, isOpen) {
     /*
      * nacitanie valve zo svg, podla id a / 
      */
-    
+
     /*funkcia na nastavenie farby  valve podla bool hodnoty a id ventila*/
-    
-    setColorValve = function(a, isOpen){
+
+    this.setColorValve = function setColorValve(a, s, isOpen) {
         var color = (isOpened === true) ? "green" : "red";
         s.select(a).attr({fill: color});
         /*zmena atributu - a je css selector - a menim attribut farby*/
     };
-    
-    
+
+
 }
 
-function ComponetPipe(a){
-    
+function ComponetPipe(a) {
+
 }
 
-function ComponetEngine(a, isOn){
-    
-    
+function ComponetEngine(a, isOn) {
+
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -36,8 +47,8 @@ function ComponetEngine(a, isOn){
 /**kvazi objekty v javascripte / inicializovane cez konstruktor prikazom new */
 var nadrz, motor, pipe, valve;
 
-function initSchema01(){
-    
+function initSchema01() {
+
     var schema01Paper = new Snap("#svg");
     /*
      * nacitanie jednotlivych komponentov cez funkcie / a cez svg.. 
@@ -45,21 +56,21 @@ function initSchema01(){
      */
 }
 
-function updateSchema01(hodnota1, hodnota2, hodnota3){
+function updateSchema01(hodnota1, hodnota2, hodnota3) {
     /*
      * volam z atributu funkciu 
      * napr valveIn.setOpened(isOpened);
      */
-        
+
 }
 
-function onPageLoad(){
+function onPageLoad() {
     initSchema01();
 }
 
 
 //na overenie funkcnosti / bud cez prikazovy riadok javascriptu alebo tlacidlami
-function toggleDemoStart(){
+function toggleDemoStart() {
     var button = document.getElementById("demo");
     /*
      * volanie funkcie updateSchema01 s roznymi parametrami a podmienkami if...
