16f0492c9a830263fa3e5658436fe055f3893596
Chovancova Olga <Olga.Chovancova@st.fri.uniza.sk>
2015-03-17 13:04:21 +0100

    male zmeny
    refaktoring

--- public_html/precerpavacia_stanica/precerp_stanica.js
+++ public_html/precerpavacia_stanica/precerp_stanica.js
@@ -10,68 +10,6 @@ function setColorValve(isOpened) {
     return  console.log("farba ventila sa zmenila " + colorValve);
 }
 
-
-
-function ComponentTank(a, s, fillPerc, cislo1, cislo2, rychlostVMs) {
-    /*
-     * nacitanie valve zo svg, podla id a / 
-     * zistenie cisla1, cisla2 zo svg
-     */
-    this.rychlostVMs = 800;
-    this.fillPerc = fillPerc;
-
-
-    this.vyska = cislo1 * (this.fillPerc / 100);
-    var vyska;
-    this.py = cislo2 - vyska;
-
-    this.animateTank = function animateTank() {
-        s.select(a).animate({height: vyska, y: py, x: cislo1}, rychlostVMs);
-    };
-
-    this.nastavNadrzNaPozadovanePercento = function nastavNadrz(percento) {
-        this.vyska = cislo1 * (percento / 100);
-    };
-
-    this.setValue = function setValue(hodnota) {
-        if (!(0 <= hodnota && 100 >= hodnota) || hodnota === undefined || null === hodnota) {this.fillPerc = 0;}
-        else {
-            if (hodnota < 100) {
-                this.fillPerc = hodnota;
-            } else {
-                this.fillPerc = 100;
-            }
-        }
-    };
-
-}
-
-function ComponentValve(a, isOpen) {
-    /*
-     * nacitanie valve zo svg, podla id a / 
-     */
-
-    /*funkcia na nastavenie farby  valve podla bool hodnoty a id ventila*/
-/*
-    this.setColorValve = function setColorValve(a, isOpen) {
-        var color = (true === isOpened) ? "green" : "red";
-        s.select(a).attr({fill: color});
-        //zmena atributu - a je css selector - a menim attribut farby//
-    };
-*/
-
-}
-
-function ComponetPipe(a) {
-
-}
-
-function ComponetEngine(a, isOn) {
-
-
-}
-
-
 /**kvazi objekty v javascripte / inicializovane cez konstruktor prikazom new */
 var nadrz = "#hladina1";/*toto je modra hladina nadrze*/
 var motor;
@@ -84,26 +22,67 @@ var s;
 function initSchema01() {
     s = Snap("#svgStanica");
     Snap.load("stanica2.svg", function (f) {
-
          s.add(f.selectAll("#stanica"));
-
     });
-
-    /*
-     * nacitanie jednotlivych komponentov cez funkcie / a cez svg.. 
-     * 
-     */
 }
 
-function updateSchema01(hodnota1, hodnota2, hodnota3) {
+/**- je otvoreny/zatvoreny ventil
+ - stav hladiny v nadrzi v percentach(dajme tomu)
+ - je zapnuty/vypnuty motor
+ - zaminovat tok vody cez rurky  - funkcia - preteka voda hore, dole (prechodRurami(1, 0);) alebo to bude nastavene logicky - ak bude zapnuty motor, tak preteka horna, ak bude otvoreny ventil tak bude pretekat dolnou.. .
+ napr. updateSchema(1, 10, 1, 1, 1); updateSchema(1, 15, 0, 1, 0); alebo update(1, 20, 0,);... */
+function updateSchema01(boolVentil, boolMotor, intHladina) {
     /*
      * volam z atributu funkciu 
      * napr valveIn.setOpened(isOpened);
      */
     alert("hodnota1: " + hodnota1 + "-------hodnota2: " + hodnota2 + " -----hodnota3: " + hodnota3);
+}
+
+
+
+function animateComponentTank(fillPerc) {
+    var rychlostVMs = 800;
+    var cislo1= 0;
+    var cislo2 = 0;///zistit suradnice x, y
+    var percento = fillPerc;
+
+    this.vyska = cislo1 * (fillPerc / 100);
+    this.py = cislo2 - cislo1 * (fillPerc / 100);
+
+    this.animateTank = function animateTank() {
+        s.select(nadrz).animate({
+            height: (cislo1 * (fillPerc / 100)),
+            y: cislo2 - cislo1 * (fillPerc / 100),
+            x: cislo1
+        }, rychlostVMs);
+    };
+}
+
+function ComponentValve(a, isOpen) {
+    /*
+     * nacitanie valve zo svg, podla id a /
+     */
+
+    /*funkcia na nastavenie farby  valve podla bool hodnoty a id ventila*/
+    /*
+     this.setColorValve = function setColorValve(a, isOpen) {
+     var color = (true === isOpened) ? "green" : "red";
+     s.select(a).attr({fill: color});
+     //zmena atributu - a je css selector - a menim attribut farby//
+     };
+     */
 
 }
 
+function ComponetPipe(a) {
+
+}
+
+function ComponetEngine(a, isOn) {
+
+
+}
 
 
 
