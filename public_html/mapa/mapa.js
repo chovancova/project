adee590b8ac4c9221ac121959cd819f265772525
Chovancova Olga <Olga.Chovancova@st.fri.uniza.sk>
2015-04-19 19:28:57 +0200

    praca na kap3. a na mape

--- public_html/mapa/mapa.js
+++ public_html/mapa/mapa.js
@@ -18,6 +18,16 @@ var init = function(id, subor) {
 var textBA ="#textBratislava";
 var textNO =  "#textNamestovo";
 var textZA = "#textZilina";
+var textKE = "#textKosice";
+
+var BC = "#SK-BC";
+var BL = "#SK-BL";
+var KI = "#SK-KI";
+var NI = "#SK-NI";
+var PV = "#SK-PV";
+var TA = "#SK-TA";
+var TC = "#SK-TC";
+var ZI = "#SK-ZI";
 
 
 var zmenaVlastnostiTextu = function (nazovIDelementu) {
@@ -33,9 +43,45 @@ var nazov = nazovIDelementu;
         });
     };
 
- function test(){
+var zmenaFarbyKraja = function(okr, p_farba){
+
+    var farba = p_farba;
+    paper.selectAll(okr).attr({
+        strokeWidth:2,
+        stroke:"#000",
+        fill: p_farba
+    });
+    //console.log("chyy");
+};
+
+var spustiTesty = function spustTesty(){
+    testZmenaVlastnosti();
+    testZmenyFarbyKraja();
+}
+
+function testNaGroup(){
+    g = Snap().g();
+    g.g(paper.select(textBA), paper.select(textKE));
+    g.attr({"fill": "blue"});
+}
+
+ function testZmenaVlastnosti(){
     zmenaVlastnostiTextu(textBA);
      zmenaVlastnostiTextu(textNO);
      zmenaVlastnostiTextu(textZA);
+     zmenaVlastnostiTextu(textKE);
 }
+
+function testZmenyFarbyKraja(){
+    zmenaFarbyKraja(BC, "#00a");
+    zmenaFarbyKraja(BL,"#0ff");
+    zmenaFarbyKraja(KI, "#0fa");
+    zmenaFarbyKraja(NI, "#faa");
+    zmenaFarbyKraja(PV, "#f00");
+    zmenaFarbyKraja(TA,"#abc");
+    zmenaFarbyKraja(TC, "#0f0");
+    zmenaFarbyKraja(ZI, "#f0a");
+
+}
+
 
