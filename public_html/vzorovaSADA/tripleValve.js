abe6b07e54af9e9b59dd840ee0265783b80d8699
Chovancova Olga <Olga.Chovancova@st.fri.uniza.sk>
2015-04-24 09:12:35 +0200

    HOTOVA ROTACIA TRIPLE VALVE
    zatial je to urobene, ze iba jedno z nich moze byt zaroven ist.. ale to
    potom doladim ...

--- public_html/vzorovaSADA/tripleValve.js
+++ public_html/vzorovaSADA/tripleValve.js
@@ -1,3 +1,48 @@
 /**
  * Created by chova_000 on 24-Apr-15.
  */
+
+var paper;
+var isPaused = true;
+var animationRunning = false;
+
+function onPageLoad() {
+    paper = Snap("#svg1");
+    Snap.load("Triple Valve.svg", function (f) {
+        paper.append(f);
+        console.log("rotor svg loaded ...");
+    });
+}
+
+function rotuj1(){
+    toggleRotation("#engineMotor");
+}
+function rotuj2(){
+    toggleRotation("#engineMotor1");
+}
+
+
+function toggleRotation(nazov) {
+    if (!animationRunning && isPaused) {
+        isPaused = false;
+        rotateLeft(paper.select(nazov));
+
+    } else {
+        isPaused = true;
+    }
+}
+
+function rotateLeft(element) {
+    animationRunning = true;
+    var rotacia = "R0,"+ element.getBBox().cx + ","+ element.getBBox().cy ;
+    console.log(rotacia);
+    element.transform(rotacia);
+    if (!isPaused) {
+        var rotacia = "R360,"+ element.getBBox().cx + ","+ element.getBBox().cy ;
+        element.animate({ transform: rotacia},2000, mina.linear, rotateLeft.bind(null, element));
+    } else {
+        animationRunning = false;
+    }
+}
+
+
